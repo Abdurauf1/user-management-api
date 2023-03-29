87ec1fe1dd4bf1319fdf48fa128658112a2c8c62
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-03-30 04:29:04 +0500

    refactor: devided code to modules

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
 const cors = require("cors");
-const { sha3_256 } = require("js-sha3");
+const usersRoutes = require("./routes/userRoutes");
+
 const port = 5001;
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "users_db",
-});
-
-db.connect(err => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log("Database connected");
-});
 
 app.use(express.json());
 app.use(cors());
 
-// register user
-app.post("/api/register", async (req, res) => {
-  const { name, email } = req.body;
-  const password = await bcrypt.hash(req.body.password, 10);
-  const sqlInsert = "INSERT INTO users_table (name, email, password) VALUES (?, ?, ?);";
-  db.query(sqlInsert, [name, email, password], error => {
-    if (error) {
-      console.log(error);
-    }
-  });
-});
+app.use("/api", usersRoutes);
 
-// getting data from database
-app.get("/api/get", (req, res) => {
-  const sqlGet = "SELECT * FROM users_table";
-  db.query(sqlGet, (error, result) => {
-    res.send(result);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-app.listen(port);
