86566d5ac76be67b626ab30b073afc79f484635a
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-04-12 03:04:05 +0500

    feat: add user status and delete function

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -9,9 +9,33 @@ const getUsers = (req, res) => {
   });
 };
 
+// set user status
+const userStatus = (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+  const sqlUpdate = "UPDATE users_table SET status = ? WHERE id = ?";
+  db.query(sqlUpdate, [status, id], (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+    res.send(status);
+  });
+};
+
+// delete user
+const deleteUser = (req, res) => {
+  const { id } = req.params;
+  const sqlRemove = "DELETE FROM users_table WHERE id = ?";
+  db.query(sqlRemove, [id], (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+};
+
 // register user
 const registerUser = async (req, res) => {
-  const { name, email } = req.body;
+  const { name, email, reg_time, login_time, status } = req.body;
   const password = await bcrypt.hash(req.body.password, 10);
 
   const sqlSelect = "SELECT * FROM users_table WHERE email = ?";
@@ -21,40 +45,53 @@ const registerUser = async (req, res) => {
       return;
     }
     if (result.length > 0) {
-      res.send({ success: false, status: "User already exists" });
+      res.send({ success: false, message: "User already exists" });
     } else {
-      const sqlInsert = "INSERT INTO users_table (name, email, password) VALUES (?, ?, ?);";
-      const values = [name, email, password];
+      const sqlInsert =
+        "INSERT INTO users_table (name, email, password, reg_time, login_time, status) VALUES (?, ?, ?, ?, ?, ?);";
+      const values = [name, email, password, reg_time, login_time, status];
       db.query(sqlInsert, values, error => {
         if (error) {
           console.log(error);
         }
       });
-      res.send({ success: true, status: "User registered successfully" });
+      res.send({ success: true, message: "User registered successfully" });
     }
   });
 };
 
 // login user
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, login_time } = req.body;
 
   const sqlSelect = "SELECT * FROM users_table WHERE email = ?";
+  const sqlUpdate = "UPDATE users_table SET login_time = ? WHERE email = ?";
   db.query(sqlSelect, [email], async (error, result) => {
     if (error) {
       console.log(error);
       return;
     }
-    if (await bcrypt.compare(password, result[0].password)) {
-      res.send({ success: true, data: result[0] });
+    if (result.length > 0) {
+      if (await bcrypt.compare(password, result[0].password)) {
+        db.query(sqlUpdate, [login_time, email], (error, result) => {
+          if (error) {
+            console.log(error);
+          }
+        });
+        res.send({ success: true });
+      } else {
+        res.send({ success: false, message: "Invalid Password" });
+      }
     } else {
-      res.send({ success: false, status: "Invalid Password" });
+      res.send({ success: false, message: "User does not exists" });
     }
   });
 };
 
 module.exports = {
+  deleteUser,
   getUsers,
   registerUser,
   loginUser,
+  userStatus,
 };
