e5688a43a1a804492d23628a5631e0a6d211f1e0
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-07-28 16:33:35 +0500

    refactor: changed mysql db to ongodb

--- database/db.js
+++ database/db.js
@@ -1,20 +1,16 @@
-const mysql = require("mysql");
+const mongoose = require("mongoose");
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_DATABASE } = process.env;
+const database = DB_DATABASE;
 
-const db = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-});
-
-db.connect(err => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log("Database connected");
-});
-
-module.exports = db;
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(error => {
+    console.log(error);
+  });
