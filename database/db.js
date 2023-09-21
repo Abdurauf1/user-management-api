89b5b66f2b9bf6437cde527a6b7f4091da825ac7
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-09-21 11:07:42 +0500

    feat: connect database

---
+++ database/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const { DB_URL } = process.env;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Connected to Database");
+});
+
+module.exports = db;
