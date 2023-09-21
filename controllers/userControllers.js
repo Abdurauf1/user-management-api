06e02a73e330c2221be38fff69e7d5d497fee650
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-09-21 11:11:15 +0500

    refactor

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,36 +1,8 @@
 const bcrypt = require("bcryptjs");
-
-// get users from database
-const getUsers = (req, res) => {};
-
-// set user status
-const userStatus = (req, res) => {
-  //   const { id } = req.params;
-  //   const { status } = req.body;
-  //   const sqlUpdate = "UPDATE users_table SET status = ? WHERE id = ?";
-  //   db.query(sqlUpdate, [status, id], (error, result) => {
-  //     if (error) {
-  //       console.log(error);
-  //     }
-  //     res.send(status);
-  //   });
-};
-
-// delete user
-const deleteUser = (req, res) => {
-  //   const { id } = req.params;
-  //   const sqlRemove = "DELETE FROM users_table WHERE id = ?";
-  //   db.query(sqlRemove, [id], (error, result) => {
-  //     if (error) {
-  //       console.log(error);
-  //     }
-  //   });
-};
+const User = require("../models/User");
 
 // register user
 const registerUser = async (req, res) => {
-  // const { name, email, reg_time, login_time, status } = req.body;
-  // const password = await bcrypt.hash(req.body.password, 10);
   // const sqlSelect = "SELECT * FROM users_table WHERE email = ?";
   // db.query(sqlSelect, [email], (error, result) => {
   //   if (error) {
@@ -78,6 +50,40 @@ const loginUser = async (req, res) => {
   //     res.send({ success: false, message: "User does not exists" });
   //   }
   // });
+};
+
+// get users from database
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    console.error("Error fetching users:", err);
+  }
+};
+
+// set user status
+const userStatus = async (req, res) => {
+  //   const { id } = req.params;
+  //   const { status } = req.body;
+  //   const sqlUpdate = "UPDATE users_table SET status = ? WHERE id = ?";
+  //   db.query(sqlUpdate, [status, id], (error, result) => {
+  //     if (error) {
+  //       console.log(error);
+  //     }
+  //     res.send(status);
+  //   });
+};
+
+// delete user
+const deleteUser = async (req, res) => {
+  //   const { id } = req.params;
+  //   const sqlRemove = "DELETE FROM users_table WHERE id = ?";
+  //   db.query(sqlRemove, [id], (error, result) => {
+  //     if (error) {
+  //       console.log(error);
+  //     }
+  //   });
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
 const usersRoutes = require("./routes/userRoutes");
+const express = require("express");
+const cors = require("cors");
+const db = require("./database/db");
 
 const { PORT } = process.env;
-const port = PORT | 5001;
+const app = express();
 
 app.use(express.json());
 app.use(cors());
-
 app.use("/api", usersRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
