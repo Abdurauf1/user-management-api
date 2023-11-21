5f4417d1a0b95ac4ba93b90593594285014e777e
Abdurauf <aabdurauf004@gmail.com>
2023-11-21 21:40:07 +0500

    refactor

--- database/db.js
+++ database/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const { DB_URL } = process.env;
 
-mongoose.connect(DB_URL);
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
